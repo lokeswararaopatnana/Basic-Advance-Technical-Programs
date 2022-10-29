01b33e56cf696ad9bd87f8c97807515471e9fe30
PATNANA LOKESWARA RAO <109509575+Lokeshpatnana@users.noreply.github.com>
2022-10-29 11:00:11 +0530

    Add files via upload

---
+++ 02_calendar.py
@@ -0,0 +1,6 @@
+""" Calendar by using Python """
+
+import calendar
+a=calendar.TextCalendar()
+print(a.pryear(2022))
+print(a.prmonth(2022, 12))
---
+++ 03_Even&Odd.py
@@ -0,0 +1,5 @@
+a=int(input("Enter Numbers"))
+if (a%2==0):
+    print(f"{a} is Even")
+else:
+    print(f"{a} is Odd")
